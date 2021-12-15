e6103a019a960b8f39179794ab8bb86ae7804c7c
Glonky <glonky.sailor@gmail.com>
2021-12-15 13:19:39 -0800

    feat: update next.js config for building on netlify
    feat: update next.js config for svgs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  generateBuildId: () => 'build', // This makes deploying to Netlify much faster
   reactStrictMode: true,
-}
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
