ca8491cbda248acbe39e4113065cf01e5e1915c9
Glonky <glonky.sailor@gmail.com>
2021-12-15 13:46:17 -0800

    feat: add jest
    feat: add github workflow for jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  '**/*.{ts,tsx,js}': (filenames) =>
-    `next lint --fix --file ${filenames
+  '**/*.{ts,tsx,js}': [
+    (filenames) =>`next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(' --file ')}`,
+    "yarn test:ci --findRelatedTests --coverage=false"
+  ],
 }
